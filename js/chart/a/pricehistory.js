cbc507b00f113174b2958c25bcbc77844fa66e85
Reza <rm.biqarar@gmail.com>
2020-04-04 12:22:17 +0430

    add pricehistory chart

---
+++ js/chart/a/pricehistory.js
@@ -0,0 +1,40 @@
+function chartDrawer()
+{
+  if($("#chartdiv").length == 1){myChartProductPrice();}
+}
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------------
+function myChartProductPrice()
+{
+
+  Highcharts.chart('chartdiv',
+  {
+
+    title:
+    {
+      text: $("#charttitle").text()
+    },
+     xAxis:
+    [{
+        categories: $.parseJSON($("#chartcategory").text()),
+        crosshair: true
+    }],
+
+    yAxis: [{ // Primary yAxis
+      title: {
+        text: $("#charttitleprice").text(),
+        useHTML: Highcharts.hasBidiBug,
+        style: {
+          color: Highcharts.getOptions().colors[0]
+        }
+      }
+    }],
+
+    series: $.parseJSON($("#chartseries").text()),
+
+  });
+}
