a312525f65253be02421c4e0d384f72e16aa4958
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-04 13:03:12 +0430

    add js

--- js/siftal/src/utility/googleAnalytics.js
+++ js/siftal/src/utility/googleAnalytics.js
@@ -1,2 +1,16 @@
-
-
+window.dataLayer = window.dataLayer || [];
+var myGID = $('#gAnalyticsScript').attr('data-uid');
+if(myGID)
+{
+	function gtag()
+	{
+		dataLayer.push(arguments);
+	}
+	gtag('js', new Date()); gtag('config', myGID);
+	function pushStateGA()
+	{
+		var origin = window.location.protocol + '//' + window.location.host;
+		var pathname = window.location.href.substr(origin.length);
+		gtag('config', myGID, {'page_path': pathname});
+	}
+}
