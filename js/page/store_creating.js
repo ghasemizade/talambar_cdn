624d020441621acc88fcce213be38aec684a5183
Reza <rm.biqarar@gmail.com>
2020-04-04 12:35:19 +0430

    add store creating js

---
+++ js/page/store_creating.js
@@ -0,0 +1,29 @@
+function pushStateFinal()
+{
+	$.ajax(
+	{
+	 url : $("#urlthat").text(),
+	 type : 'post',
+	 data : {"create":"store"},
+	 dataType: 'json',
+	 success : function()
+	 {
+	  Navigate({ url: $("#urlthis").text() + "/opening" });
+	 },
+	 done : function()
+	 {
+	  Navigate({ url: $("#urlthis").text() + "/opening" });
+	 },
+	 fail : function()
+	 {
+	  Navigate({ url: $("#urlthis").text() + "/error" });
+	 },
+	 statusCode: {
+	  501: function() {
+	    Navigate({ url: $("#urlthis").text() + "/error" });
+	  }
+	 }
+	});
+
+	$("#linkmhe").delay( (60 * 1000) ).fadeIn( 400 );
+}
