346c819e81b42ec3532694e7775e57bd584aff28
Reza <rm.biqarar@gmail.com>
2020-04-12 13:37:33 +0430

    add domain home page chart

---
+++ js/chart/management/domainhomepage.js
@@ -0,0 +1,100 @@
+function chartDrawer()
+{
+
+  if($("#chartdivdomain").length == 1){chartdivdomain();}
+  if($("#chartdiv").length == 1){chartdiv();}
+
+}
+
+
+
+
+
+
+
+
+
+
+function chartdivdomain()
+{
+
+  Highcharts.chart('chartdivdomain',
+  {
+    title: { text: $("#chartdomaintitle").text() },
+    xAxis: [{
+      categories: $.parseJSON($("#chartdomaincategory").text()),
+      crosshair: true
+    }],
+    yAxis: [{ title: false }],
+    series:
+    [
+      {
+        name: $("#charttitleregister").text(),
+        data: $.parseJSON($("#chartdomaincountregister").text()),
+        type: 'line',
+        showInLegend: false,
+        tooltip: {
+          valueSuffix: ' '+ $("#charttitlecount").text()
+        }
+      },
+       {
+        name: $("#charttitlerenew").text(),
+        data: $.parseJSON($("#chartdomaincountrenew").text()),
+        type: 'line',
+        showInLegend: false,
+        tooltip: {
+          valueSuffix: ' '+ $("#charttitlecount").text()
+        }
+      },
+       {
+        name: $("#charttitletransfer").text(),
+        data: $.parseJSON($("#chartdomaincounttransfer").text()),
+        type: 'line',
+        showInLegend: false,
+        tooltip: {
+          valueSuffix: ' '+ $("#charttitlecount").text()
+        }
+      }
+
+    ]
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+function chartdiv()
+{
+
+  Highcharts.chart('chartdiv',
+  {
+    title: { text: $("#chartlogtitle").text() },
+    xAxis: [{
+      categories: $.parseJSON($("#chartlogcategory").text()),
+      crosshair: true
+    }],
+    yAxis: [{ title: false }],
+    series:
+    [
+      {
+        name: $("#charttitlelog").text(),
+        data: $.parseJSON($("#chartlogcount").text()),
+        type: 'area',
+        showInLegend: false,
+        tooltip: {
+          valueSuffix: ' ' + $("#charttitlecount").text()
+        }
+      }
+    ]
+  });
+}
+
+
+
