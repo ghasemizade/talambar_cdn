da2751a7d1fe0f3b3fce0647f0acef9e9f529693
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-23 15:00:45 +0430

    fix copy

--- js/siftal/src/libs/dataCopy/dataCopy.js
+++ js/siftal/src/libs/dataCopy/dataCopy.js
@@ -4,48 +4,41 @@ function dataCopy()
   $('[data-copy]').off('click');
   $('[data-copy]').on('click', function()
   {
-    var $this = $(this);
-    var targetEl = $($this.attr('data-copy'));
-    if(targetEl.length)
+    var copyAttr = $(this).attr('data-copy');
+    if(copyAttr)
     {
-      // targetEl.attr('disabled', null);
 
-      if (navigator.userAgent.match(/ipad|ipod|iphone/i))
-      {
-        var el = targetEl.get(0);
-        var editable = el.contentEditable;
-        var readOnly = el.readOnly;
-        el.contentEditable = true;
-        el.readOnly = false;
-        var range = document.createRange();
-        range.selectNodeContents(el);
-        var sel = window.getSelection();
-        sel.removeAllRanges();
-        sel.addRange(range);
-        el.setSelectionRange(0, 999999);
-        el.contentEditable = editable;
-        el.readOnly = readOnly;
-      }
-      else
-      {
-        targetEl.select();
-      }
+      const el = document.createElement('textarea');  // Create a <textarea> element
+      el.value = copyAttr;                            // Set its value to the string that you want copied
+      el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
+      el.style.position = 'absolute';
+      el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+      document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+      const selected =
+        document.getSelection().rangeCount > 0        // Check if there is any content selected previously
+          ? document.getSelection().getRangeAt(0)     // Store selection if found
+          : false;                                    // Mark as false to know no selection existed before
+      el.select();                                    // Select the <textarea> content
 
       try
       {
+        // Copy - only works as a result of a user action (e.g. click events)
         // copy to clipboard
         document.execCommand('copy');
-        targetEl.blur();
-        notif('info', 'Copied!');
-
-        // // copied animation
-        // $this.addClass('copied');
-        // setTimeout(function() { $this.removeClass('copied'); }, 300);
+        notif('info', myText + ' copied to clipboard!');
       }
       catch (err)
       {
         console.log('cant copy! Ctrl/Cmd+C to copy')
       }
+
+      document.body.removeChild(el);                  // Remove the <textarea> element
+      if (selected) {                                 // If a selection existed before copying
+        document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
+        document.getSelection().addRange(selected);   // Restore the original selection
+      }
+
+
     }
   })
 }
