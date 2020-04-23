d52e334078d80564c1de229b8a8cca7ce1896b22
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-23 20:18:15 +0430

    add domain review

---
+++ js/page/my_domain_review.js
@@ -0,0 +1,79 @@
+
+function pageScript()
+{
+  $('[data-budget] input[type="checkbox"]').on("click", function(event)
+  {
+    calcDomainPrice();
+  });
+}
+
+
+function calcDomainPrice()
+{
+  var $price      = $('[data-price]');
+  var $gift       = $('[data-gift]');
+  var $budget     = $('[data-budget]');
+  var $payable    = $('[data-payable] #domainPayablePrice');
+  var $useBudget  = $('[data-budget] input[type="checkbox"]');
+
+  var myPrice     = 0;
+  var myGift      = 0;
+  var myBudget    = 0;
+  var useMyBudget = null;
+  var myPayable   = 0;
+
+  if($price.length === 1)
+  {
+    myPrice = $price.attr('data-price');
+  }
+
+  if($gift.length === 1)
+  {
+    myGift = $gift.attr('data-gift');
+  }
+
+  if($budget.length === 1)
+  {
+    myBudget = $budget.attr('data-budget');
+  }
+  if($useBudget.length === 1)
+  {
+    if($useBudget.is(":checked"))
+    {
+      useMyBudget = true;
+    }
+    else
+    {
+      useMyBudget = false;
+    }
+  }
+
+  // calc payable without gift
+  myPayable = myPrice - myGift;
+
+  if(useMyBudget)
+  {
+    myPayable = myPayable - myBudget;
+    if(myPayable < 0)
+    {
+      myPayable = 0;
+    }
+  }
+
+  if($payable.length === 1)
+  {
+    $payable.text(fitNumber(myPayable));
+  }
+
+  console.log(myPrice);
+  console.log(myGift);
+  console.log(myBudget);
+  console.log(useMyBudget);
+  console.log(myPayable);
+
+
+}
+
+$(document).ready(function(){
+  pageScript();
+});
