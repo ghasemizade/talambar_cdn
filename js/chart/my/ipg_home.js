5badb2f005640687d86a5b163fcfa57587c0ae5d
Reza <rm.biqarar@gmail.com>
2020-04-28 12:34:11 +0430

    add ipg home chart

---
+++ js/chart/my/ipg_home.js
@@ -0,0 +1,37 @@
+function pageChart()
+{
+
+  if($("#chartdiv").length == 1){chartdiv();}
+
+}
+
+
+
+function chartdiv()
+{
+
+  Highcharts.chart('chartdiv',
+  {
+    title: { text: $("#charttitle").text() },
+    xAxis: [{
+      categories: $.parseJSON($("#chartcategory").text()),
+      crosshair: true
+    }],
+    yAxis: [{ title: false }],
+    series:
+    [
+      {
+        name: $("#charttitlepayed").text(),
+        data: $.parseJSON($("#chartprice").text()),
+        type: 'column',
+        showInLegend: false,
+        tooltip: {
+          valueSuffix: ' ' + $("#charttitleprice").text()
+        }
+      }
+    ]
+  });
+}
+
+
+
