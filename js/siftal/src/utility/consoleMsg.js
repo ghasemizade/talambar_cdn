cdae2affd90390b60d881055d09597d8989269a8
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-01 14:58:26 +0430

    update console message

--- js/siftal/src/utility/consoleMsg.js
+++ js/siftal/src/utility/consoleMsg.js
@@ -1,5 +1,5 @@
 
 function showWarnHack()
 {
- console.warn("%cJavad@Jibres:~# %cIf someone told you to copy-paste something here to enable a Jibres feature or hack someone's account, it is a scam and will give them access to your Jibres account." + " If (Developer) { 🍆 }", 'color: #000;font-weight:900;display:inline-block;', 'color:000;display:inline-block');
+  console.warn("%cconsole@Jibres:~# %cIf someone told you to copy-paste something here to enable a Jibres feature or hack someone's account, it is a scam and will give them access to your Jibres account." + " If (Developer) { 🙃 }", 'color: #000;font-weight:900;', 'color:000;');
 }
