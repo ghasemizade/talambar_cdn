32667cbd881905c2c5123929c232fe99e4319a92
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-10 17:29:16 +0430

    bugfix shortkey on editor

--- js/siftal/src/libs/shortkey/shortkey.js
+++ js/siftal/src/libs/shortkey/shortkey.js
@@ -52,15 +52,17 @@ function shortkey_corridor(_e, _self, _key)
     return null;
   }
 
-
   // logy(mytxt, 'info');
 
   var elShortkey = $('[data-shortkey="'+ mytxt +'"]');
+  var $focused = $(':focus');
+
+  // on editor disable shortkey
+  if($focused.parents('.ck-editor').length > 0) return;
 
   // try to search for combine mode
   if(elShortkey.length == 0)
   {
-    var $focused = $(':focus');
     if(lastKeyPressed && !$focused.is('input'))
     {
       var mytxt2 = lastKeyPressed + "+" + mytxt;
