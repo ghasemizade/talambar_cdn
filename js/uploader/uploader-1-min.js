eb4df15a13c109d9910d0289c08d70aa37b14bd1
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-21 10:15:15 +0430

    check upload features is enable or not

--- js/uploader/uploader-1-min.js
+++ js/uploader/uploader-1-min.js
@@ -2,7 +2,23 @@
 function runUploader()
 {
   console.log('run uploader');
+  // define variables
+  var myUploaderFrame = $('[data-uploader]');
   var myInput = $('[data-uploader] input[type="file"]')
+
+
+  // check advance upload is enable or not
+  var isAdvancedUpload = function(_el)
+  {
+    return ( ( 'draggable' in _el ) || ( 'ondragstart' in _el && 'ondrop' in _el ) ) && 'FormData' in window && 'FileReader' in window;
+  }();
+
+  if(!isAdvancedUpload(myUploaderFrame))
+  {
+    console.log('Advance upload features is not enable on browser.');
+    return false;
+  }
+
 
   myInput.off('change').on('change', function(_e){
     console.log(_e);
