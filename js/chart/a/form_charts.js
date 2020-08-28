0f8abd53eadc7a725fd90c858ba42d99e25a6c40
Reza <rm.biqarar@gmail.com>
2020-08-28 15:27:30 +0430

    Fix form chart js

---
+++ js/chart/a/form_charts.js
@@ -0,0 +1,73 @@
+function chart_a_form_charts()
+{
+  if($("#chartdivpie").length == 1){highChart_pie();}
+  if($("#chartdivbar").length == 1){highChart_bar();}
+
+}
+
+
+
+
+function highChart_pie()
+{
+
+ Highcharts.chart('chartdivpie', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: $('#chartitemtitle').html()
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f}'
+            }
+        }
+    },
+    series: [{
+        name: $('#chartitemtitle').html(),
+        colorByPoint: true,
+        data: $.parseJSON($("#chartdata").text())
+    }]
+});
+}
+
+
+
+function highChart_bar()
+{
+
+ Highcharts.chart('chartdivbar', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'column'
+    },
+    title: {
+        text: $('#chartitemtitle').html()
+    },
+    plotOptions: {
+        column: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f}'
+            }
+        }
+    },
+    series: [{
+        name: $('#chartitemtitle').html(),
+        colorByPoint: true,
+        data: $.parseJSON($("#chartdata").text())
+    }]
+});
+}
