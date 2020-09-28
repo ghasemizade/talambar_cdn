6713c318adcbc36fa437e2d41645f6f8f8df4497
Reza <rm.biqarar@gmail.com>
2020-09-28 17:16:29 +0330

    Fix creating store

--- js/page/store_creating.js
+++ js/page/store_creating.js
@@ -1,30 +1,6 @@
 function pageScript()
 {
-	$.ajax(
-	{
-	 url : $("#urlthat").text(),
-	 type : 'post',
-	 data : {"create":"store"},
-	 dataType: 'json',
-	 success : function()
-	 {
-	  Navigate({ url: $("#urlthisopening").text() });
-	 },
-	 done : function()
-	 {
-	  Navigate({ url: $("#urlthisopening").text() });
-	 },
-	 fail : function()
-	 {
-	  Navigate({ url: $("#urlthiserror").text() });
-	 },
-	 statusCode: {
-	  501: function() {
-	    Navigate({ url: $("#urlthiserror").text() });
-	  }
-	 }
-	});
-
+	$("#createstoreform").submit();
 	$("#linkmhe").delay( (60 * 1000) ).fadeIn( 400 );
 }
 
