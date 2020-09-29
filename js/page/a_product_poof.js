52db9ab244d7f722733507d4be3d2474305b7c09
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-09-29 20:20:56 +0330

    clean code

--- js/page/a_product_poof.js
+++ js/page/a_product_poof.js
@@ -3,100 +3,12 @@ function pageScript()
 {
   var $showEl = $('#poofImageFetch');
 
-  var $q = $showEl.attr('data-title');
-  // https://cors-anywhere.herokuapp.com/https://example.com
-  var proxyurl = "https://cors-anywhere.herokuapp.com/";
-  // site that doesn’t send Access-Control-*
-  var url      = "https://www.digikala.com/search/?q=" + $q;
-  var dgUrl    = proxyurl + url;
-
-  fetch(dgUrl).then(function (response)
-  {
-    // The API call was successful!
-    return response.text();
-  }).then(function (html)
-  {
-    // Convert the HTML string into a document object
-    var parser = new DOMParser();
-    if(!parser)
-    {
-      console.log('parser is not defined!');
-      return false;
-    }
-    var doc = parser.parseFromString(html, 'text/html');
-    if(!doc)
-    {
-      console.log('doc is not recognized');
-      return false;
-    }
-
-    var myProductList = $(doc).find('.c-listing__items');
-    var eachItem = myProductList.find('.c-product-box');
-    // var images = myProductList.find('.js-product-item img');
-    var pTitle, pUnit, pPrice, pCompareAtPrice, pImg, data, newEl;
-
-
-
-    eachItem.each(function(index)
-    {
-      $this  = $(this);
-      pTitle = $this.find('.c-product-box__title a').text();
-      pUnit  = $this.find('.c-price .c-price__value-wrapper .c-price__currency').text();
-      pPrice = $this.find('.c-price .c-price__value-wrapper').text().replace(pUnit, '').trim();
-      pCompareAtPrice = $this.find('.c-price del').text();
-
-      // pImg = $(this).attr('src');
-      pImg = $this.find('.c-product-box__img img').attr('src');
-
-      if(pImg)
-      {
-        // create product Element
-        newEl  = "<div class='c-2'>";
-        {
-          newEl += "<div class='jProduct2' data-data='" + '{"url":"' + pImg + '"}' + "'>";
-          {
-            newEl += "<figure class='overlay'>"
-            {
-              newEl += '<img src=' + pImg + ' alt="poof">'
-              newEl += "<footer>";
-              {
-                newEl += "<figcaption>" + pTitle + "</figcaption>";
-                newEl += "<div class='f align-center'>"
-                if(pUnit)
-                {
-                  newEl += "<span class='unit cauto'>" + pUnit + "</span>";
-                }
-                if(pPrice)
-                {
-                  newEl += "<span class='price c'>"+ pPrice +"</span>";
-                }
-                if(pCompareAtPrice)
-                {
-                  newEl += "<del class='compareAtPrice cauto os'>" + pCompareAtPrice +"</del>";
-                }
-                newEl += "</div>"
-              }
-              newEl += "</footer>";
-            }
-
-            newEl += '</figure>';
-          }
-          newEl += '</div>';
-        }
-        newEl += '</div>';
-
-        $showEl.append(newEl);
-      }
-    });
-  }).catch(function (err) {
-    // There was an error
-    console.warn('Something went wrong.', err);
-  });
+  // get data from digikala and convert it to array
+  searchResult = readDigiKalaList($showEl.attr('data-title'), $showEl);
 }
 
 
-
-function readDigiKalaList(_q)
+function readDigiKalaList(_q, _target)
 {
   // site that doesn’t send Access-Control-*
   var searchUrl      = "https://www.digikala.com/search/?q=" + _q;
@@ -114,21 +26,21 @@ function readDigiKalaList(_q)
     var parser = new DOMParser();
     if(!parser)
     {
-      console.log('parser is not defined!');
+      logy('parser is not defined!');
       return false;
     }
 
     var doc = parser.parseFromString(html, 'text/html');
     if(!doc)
     {
-      console.log('doc is not recognized');
+      logy('doc is not recognized');
       return false;
     }
 
     var myProductList = $(doc).find('.c-listing__items .c-product-box');
     if(!myProductList)
     {
-      console.log('list is empty');
+      logy('list is empty');
       return false;
     }
 
@@ -150,6 +62,12 @@ function readDigiKalaList(_q)
       myResult[index] = myItem;
     });
 
+    // try to create element to show data
+    if(_target)
+    {
+      createProductListElement(myResult, _target);
+    }
+
     return myResult;
 
   }).catch(function (err) {
@@ -159,16 +77,16 @@ function readDigiKalaList(_q)
 }
 
 
-function readDigiKalaList(_datalist, _target)
+function createProductListElement(_datalist, _target)
 {
   if(!_datalist)
   {
-    console.log('digikala data list is null');
+    logy('digikala data list is null');
     return false;
   }
   if(!_target)
   {
-    console.log('target to show digikala converted list is empty');
+    logy('target to show digikala converted list is empty');
     return false;
   }
 
