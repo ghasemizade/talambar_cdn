b17626b23b116802ddca3c491df327bd4c14a67e
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-09-29 20:06:02 +0330

    add read fn

--- js/page/a_product_poof.js
+++ js/page/a_product_poof.js
@@ -2,9 +2,8 @@
 function pageScript()
 {
   var $showEl = $('#poofImageFetch');
-  var myTitle = $showEl.attr('data-title');
 
-  $q = myTitle;
+  var $q = $showEl.attr('data-title');
   // https://cors-anywhere.herokuapp.com/https://example.com
   var proxyurl = "https://cors-anywhere.herokuapp.com/";
   // site that doesn’t send Access-Control-*
@@ -94,3 +93,72 @@ function pageScript()
     console.warn('Something went wrong.', err);
   });
 }
+
+
+
+function readDigiKalaList(_q)
+{
+  // site that doesn’t send Access-Control-*
+  var searchUrl      = "https://www.digikala.com/search/?q=" + _q;
+  // https://cors-anywhere.herokuapp.com/https://example.com
+  var dgUrl    = "https://cors-anywhere.herokuapp.com/" + searchUrl;
+
+  fetch(dgUrl).then(function (response)
+  {
+    // The API call was successful!
+    return response.text();
+  }).then(function (html)
+  {
+
+    // Convert the HTML string into a document object
+    var parser = new DOMParser();
+    if(!parser)
+    {
+      console.log('parser is not defined!');
+      return false;
+    }
+
+    var doc = parser.parseFromString(html, 'text/html');
+    if(!doc)
+    {
+      console.log('doc is not recognized');
+      return false;
+    }
+
+    var myProductList = $(doc).find('.c-listing__items .c-product-box');
+    if(!myProductList)
+    {
+      console.log('list is empty');
+      return false;
+    }
+
+
+
+    var myResult = {};
+
+
+
+    myProductList.each(function(index)
+    {
+      var myItem = {};
+      var $this  = $(this);
+      // read values
+      myItem.title          = $this.find('.c-product-box__title a').text();
+      myItem.unit           = $this.find('.c-price .c-price__value-wrapper .c-price__currency').text();
+      myItem.price          = $this.find('.c-price .c-price__value-wrapper').text().replace(myItem.unit, '').trim();
+      myItem.compareAtPrice = $this.find('.c-price del').text();
+      myItem.img            = $this.find('.c-product-box__img img').attr('src');
+
+      myResult[index] = myItem;
+    });
+
+    console.log('finish');
+    console.log(myResult);
+    return myResult();
+
+  }).catch(function (err) {
+    // There was an error
+    console.warn('Something went wrong.', err);
+  });
+}
+
