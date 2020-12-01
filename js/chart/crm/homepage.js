4e71ac25b884496c69877aeffabb12f6d64480b2
Reza <rm.biqarar@gmail.com>
2020-12-01 14:31:41 +0330

    Add cms home page chart

---
+++ js/chart/crm/homepage.js
@@ -0,0 +1,62 @@
+function chart_crm_homepage()
+{
+  if($("#chartdiv").length == 1){highChart();}
+}
+
+
+
+
+function highChart()
+{
+
+  Highcharts.chart('chartdiv',
+  {
+    chart: {
+      type: 'areaspline',
+      zoomType: 'x',
+    },
+    title: {
+      text: $("#charttitle").html()
+    },
+    xAxis: [{
+      categories: $.parseJSON($("#chartcategory").html()),
+      crosshair: true
+    }],
+    yAxis: [{ // Primary yAxis
+      labels: {
+        format: '{value}',
+      },
+      title: {
+        text: $("#charttitle").html(),
+      }
+    },
+    { // Secondary yAxis
+      title: {
+        text: $("#charttitleunit").html(),
+      },
+      labels: {
+        format: '{value}',
+      },
+      opposite: true
+    }],
+    legend: {
+      layout: 'vertical',
+      align: 'left',
+      x: 120,
+      verticalAlign: 'top',
+      y: 50,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || 'rgba(255,255,255,0.25)'
+    },
+      series: [{
+        name: $("#chartverifytitle").html(),
+        data: $.parseJSON($("#chartverify").html())
+      },
+      {
+        name: $("#chartunverifytitle").html(),
+        data: $.parseJSON($("#chartunverify").html())
+      }
+      ]
+  });
+}
+
