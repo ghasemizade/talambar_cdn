0d6727af0b1e341f8fd211a6b3e728f82f17ff21
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-12-04 20:42:49 +0330

    fix style for fa version

--- js/jibres/src/utility/tawk.js
+++ js/jibres/src/utility/tawk.js
@@ -5,6 +5,26 @@
 function runTawk()
 {
 	var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-}
 
+  if($('html').attr('lang') === 'fa'){
+    setTimeout(function()
+    {
+      var iframe = $('iframe[title="chat widget"].open');
+      if(iframe.length)
+      {
+        iframe = iframe.get(0);
+        var style = document.createElement('style');
+        style.textContent =
+        "body{font-family:'IRANYekan'!important;} " +
+        "#greetingsText{font-size:12px!important;} " +
+        ".messageWrapper .message{font-size:12px!important;}" +
+        "#chatTextarea{font-size:12px!important;line-height:16px!important;} " +
+        '#contentContainer>[id*="-"]{opacity:0!important;display:none!important;} '+
+        '#chatContainerWrapper{bottom:48px!important;}'
+        ;
+        iframe.contentDocument.head.appendChild(style);
+      }
+    }, 2000);
+  }
+}
 
