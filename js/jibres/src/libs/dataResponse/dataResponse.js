07aefffa926987b27ac0ccc48252ef43b5a1976e
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-02-16 15:40:06 +0330

    set direction

--- js/jibres/src/libs/dataResponse/dataResponse.js
+++ js/jibres/src/libs/dataResponse/dataResponse.js
@@ -56,10 +56,32 @@ function textareaTypedChar(_this)
 	{
 		charCount = myText.length;
 		wordCount = myText.split(' ').length;
+		if(isRTL(myText))
+		{
+			$(_this).attr('data-dir', 'rtl');
+		}
+		else
+		{
+			$(_this).attr('data-dir', 'ltr');
+		}
 	}
+	else
+	{
+		$(_this).attr('data-dir', null);
+	}
+
+
 	$(_this).attr('data-count-char', charCount);
 	$(_this).attr('data-count-word', wordCount);
 }
+
+function isRTL(s){
+    var ltrChars    = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF'+'\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF',
+        rtlChars    = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC',
+        rtlDirCheck = new RegExp('^[^'+ltrChars+']*['+rtlChars+']');
+
+    return rtlDirCheck.test(s);
+};
 
 
 /**
