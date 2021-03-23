203eeaa30e2f21e25d6be1427cea72d768deec68
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-24 03:31:30 +0430

    add pre to editor

--- js/ckeditor5/ckeditor.js
+++ js/ckeditor5/ckeditor.js
@@ -58,7 +58,7 @@ function editorRunner(_el)
 				'alignment',
 				'imageUpload',
 				'blockQuote',
-				'codeBlock',
+				// 'codeBlock',
 				'insertTable',
 				// 'mediaEmbed',
 				'undo',
@@ -73,6 +73,15 @@ function editorRunner(_el)
 				'imageStyle:side'
 			]
 		},
+    heading: {
+      options: [
+        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+        { model: 'heading1', view: 'h2', title: 'Heading 1', class: 'ck-heading_heading1' },
+        { model: 'heading2', view: 'h3', title: 'Heading 2', class: 'ck-heading_heading2' },
+        { model: 'heading3', view: 'h4', title: 'Heading 3', class: 'ck-heading_heading3' },
+        { model: 'pre', view: 'pre', title: 'Code Block', class: 'ck-pre' },
+      ]
+    },
 		link: {
             // Automatically add target="_blank" and rel="noopener noreferrer" to all external links.
             addTargetToExternalLinks: true,
