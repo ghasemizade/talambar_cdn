ab03c54395cdd52fb56bb185ceb6c0a470624534
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-04-13 15:05:06 +0430

    improve pingi

--- js/jibres/src/libs/pingi/pingi.js
+++ js/jibres/src/libs/pingi/pingi.js
@@ -10,30 +10,39 @@ function pingi()
 
 function pingiWebsite(_firstTime)
 {
-  $.ajax(
-    {
-      url: window.location.protocol + "//" + window.location.hostname,
-      cache: false,
-      timeout: 500,
-      processData: false,
-      contentType: false,
-      // dataType: 'json',
-      data :
+  if(navigator.onLine)
+  {
+    $.ajax(
       {
-          'cmd' : 'ping',
-      },
-    })
-    .done(function(data, status, xhr)
-    {
-      console.log('We are okay!');
-      $("body").trigger("pingi:website:ok", _firstTime);
-    })
-    .fail(function(_result, _textStatus, _error)
-    {
-      console.log('fail get website with ping :|');
-      $("body").trigger("pingi:website:fail", _firstTime);
-    }
-  );
+        url: window.location.protocol + "//" + window.location.hostname,
+        cache: false,
+        timeout: 500,
+        processData: false,
+        contentType: false,
+        // dataType: 'json',
+        data :
+        {
+            'cmd' : 'ping',
+        },
+      })
+      .done(function(data, status, xhr)
+      {
+        console.log('We are okay!');
+        $("body").trigger("pingi:website:ok", _firstTime);
+      })
+      .fail(function(_result, _textStatus, _error)
+      {
+        console.log('fail get website with ping :|');
+        $("body").trigger("pingi:website:fail", _firstTime);
+      }
+    );
+  }
+  else
+  {
+    console.log('We have no internet connection!');
+    $("body").trigger("pingi:offline", _firstTime);
+  }
+
 }
 
 
@@ -42,14 +51,14 @@ function pingiRunner()
   // on fail do something
   $('body').on('pingi:website:fail', function(_e, _firstTime)
   {
-    // show notif on offline mode if we cant see website
+    // show notif if we cant see website
     if($('html').attr('lang') === 'fa')
     {
-      notif('fatal', "ما نمی‌توانیم  "+ window.location.hostname + " را ببینیم", 'ارتباط از دست رفت!', 8000, {'position':'topCenter', 'icon':'sf-plug', 'displayMode':1});
+      notif('warn', "ما نمی‌توانیم  "+ window.location.hostname + " را ببینیم", 'ارتباط از دست رفت!', 8000, {'position':'topCenter', 'icon':'sf-exclamation-circle', 'displayMode':1});
     }
     else
     {
-      notif('fatal', "We can't see "+ window.location.hostname, 'Connection is lost', 8000, {'position':'topCenter', 'icon':'sf-plug', 'displayMode':1});
+      notif('warn', "We can't see "+ window.location.hostname, 'Connection is lost', 8000, {'position':'topCenter', 'icon':'sf-exclamation-circle', 'displayMode':1});
     }
 
 
@@ -93,6 +102,37 @@ function pingiRunner()
     // remove offline mode
     $("body").attr('data-offline', null);
   });
-}
 
+
+  // on fail do something
+  $('body').on('pingi:offline', function(_e, _firstTime)
+  {
+    // show notif if we don't have internet connection
+    if($('html').attr('lang') === 'fa')
+    {
+      notif('fatal', 'اینترنت شما قطع شده است!', "لطفا ارتباط اینترنت خود را بررسی کنید", 10000, {'position':'topCenter', 'icon':'sf-plug', 'displayMode':1});
+    }
+    else
+    {
+      notif('fatal', "Check your internet connection", 'No internet!', 10000, {'position':'topCenter', 'icon':'sf-plug', 'displayMode':1});
+    }
+
+
+    // disallow to run multiple and only create one task to check
+    if(_firstTime)
+    {
+      if($("body").attr('data-offline') === undefined)
+      {
+        setTimeout(pingiWebsite, 10000);
+        // set offline mode
+        $("body").attr('data-offline', '');
+      }
+    }
+    else
+    {
+      // after a delay try to recheck connection
+      setTimeout(pingiWebsite, 10000);
+    }
+  });
+}
 
