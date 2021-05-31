74d8244609e228b380629dea91e253f3520a8e82
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-05-31 16:00:56 +0430

    resize desktop preview

---
+++ js/page/a_site_builder.js
@@ -0,0 +1,21 @@
+
+function pageScript()
+{
+  $('.browserFrame #resizePreview [data-mode]').off('click.resizePreview').on("click.resizePreview", function(event)
+  {
+    resizePreview($(this).attr('data-mode'));
+  });
+
+  function resizePreview(_to)
+  {
+    if(_to === 'desktop')
+    {
+      $('.browserFrame').attr('data-size', 'desktop');
+    }
+    else if(_to === 'mobile')
+    {
+      $('.browserFrame').attr('data-size', 'mobile');
+    }
+  }
+}
+
