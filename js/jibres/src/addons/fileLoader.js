c1f8e6003f63e1783a67e51d5693c762c2d14ee0
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-06-06 19:24:55 +0430

    update select2 style to latest version with some new fix

--- js/jibres/src/addons/fileLoader.js
+++ js/jibres/src/addons/fileLoader.js
@@ -166,7 +166,7 @@ function readPageSelect2()
   if(myEl && myEl.length > 0)
   {
     // load script
-    StyleLoader(urlJibres('cdn') + "lib/select2/select2.full-v4.0.13.css?v=1");
+    StyleLoader(urlJibres('cdn') + "lib/select2/select2.full-v4.0.13.min.css?v=1");
     myUrl = urlJibres('cdn') + "lib/select2/select2.full-v4.0.13.min.js?v=3";
     fileLoader(myUrl, 'runPageSelect2', true);
   }
