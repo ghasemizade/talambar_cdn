a52367b85ce577a1103d190e76c9157e2ca1d47f
Reza <rm.biqarar@gmail.com>
2021-07-24 14:31:52 +0430

    Add accounting factor js

---
+++ js/page/a_accounting_factor.js
@@ -0,0 +1,16 @@
+function pageScript()
+{
+	$("#factor-auto-calculate-vat").click(function(){
+
+		var total         = $("#input-total").val();
+		var totaldiscount = $("#input-totaldiscount").val();
+
+		total         = total.replace(',', '');
+		totaldiscount = totaldiscount.replace(',', '');
+
+		final         = parseInt(total) - parseInt(totaldiscount);
+
+		$("#input-totalvat").val(final * 0.9);
+
+	});
+}
