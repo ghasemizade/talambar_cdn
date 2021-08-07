731dd887701976c7bbe9c25e70dc9dc9f41d7b97
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-07 15:04:58 +0430

    remove responsive xl

--- css/tailwind/tailwind.config.js
+++ css/tailwind/tailwind.config.js
@@ -21,7 +21,7 @@ module.exports = {
       'sm': {'min': '640px'},
       'md': {'min': '768px'},
       'lg': {'min': '1024px'},
-      'xl': {'min': '1280px'},
+      // 'xl': {'min': '1280px'},
       // '2xl': {'min': '1536px'},
     },
     fontFamily: {
