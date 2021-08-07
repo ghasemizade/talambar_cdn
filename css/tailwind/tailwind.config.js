90f396cdf90923cbe1e3f66fb58a4022dfc1c0b0
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-07 15:05:22 +0430

    remove md responsive

--- css/tailwind/tailwind.config.js
+++ css/tailwind/tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
   theme: {
     screens: {
       'sm': {'min': '640px'},
-      'md': {'min': '768px'},
+      // 'md': {'min': '768px'},
       'lg': {'min': '1024px'},
       // 'xl': {'min': '1280px'},
       // '2xl': {'min': '1536px'},
