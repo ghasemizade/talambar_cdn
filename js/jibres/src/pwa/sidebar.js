e1d4386ec1d88f016d55e7a807ea55947253d321
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-10-17 17:47:34 +0330

    bugfix close sidebar on new admin panel

--- js/jibres/src/pwa/sidebar.js
+++ js/jibres/src/pwa/sidebar.js
@@ -4,35 +4,38 @@ function handlePWASlideBar()
 {
   $('body').off('mousedown.hamburger touchmove.hamburger').on('mousedown touchmove', function(_e)
   {
-    if($(_e.target).parents('.hamburger').length || $(_e.target).hasClass('hamburger') )
+    if($(_e.target).parents('html[data-pwa]').length === 1)
     {
-      // click on hanlder, do nothing!
-      if($('#sidebar').is(":visible"))
+      if($(_e.target).parents('.hamburger').length || $(_e.target).hasClass('hamburger') )
       {
+        // click on hanlder, do nothing!
+        if($('#sidebar').is(":visible"))
+        {
+          pwaSidebarHide();
+        }
+        else
+        {
+          pwaSidebarShow();
+        }
+      }
+      else if($(_e.target).parents('#sidebar').length)
+      {
+        // do nothing because clicked on sidebar
+      }
+      else if($(_e.target).is('#sidebar'))
+      {
+        // do nothing because clicked on sidebar
+        // sidebar is empty place of back
         pwaSidebarHide();
+      }
+      else if(!$('#sidebar').is(":visible"))
+      {
+        // do nothing because its hide!
       }
       else
       {
         pwaSidebarShow();
       }
-    }
-    else if($(_e.target).parents('#sidebar').length)
-    {
-      // do nothing because clicked on sidebar
-    }
-    else if($(_e.target).is('#sidebar'))
-    {
-      // do nothing because clicked on sidebar
-      // sidebar is empty place of back
-      pwaSidebarHide();
-    }
-    else if(!$('#sidebar').is(":visible"))
-    {
-      // do nothing because its hide!
-    }
-    else
-    {
-      pwaSidebarShow();
     }
   });
 }
