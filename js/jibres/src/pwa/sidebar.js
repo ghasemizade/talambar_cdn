5c6c493190e402d519fcdab2636759994f0aafa5
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-07 13:28:58 +0330

    try to fix sidebar

--- js/jibres/src/pwa/sidebar.js
+++ js/jibres/src/pwa/sidebar.js
@@ -6,8 +6,11 @@ function handlePWASlideBar()
   {
     if($(_e.target).parents('html[data-pwa]').length === 1)
     {
+      console.log('click');
+      console.log($(_e.target));
       if($(_e.target).parents('.hamburger').length || $(_e.target).hasClass('hamburger') )
       {
+        console.log(11);
         // click on hanlder, do nothing!
         if($('#sidebar').is(":visible"))
         {
@@ -20,30 +23,45 @@ function handlePWASlideBar()
       }
       else if($(_e.target).parents('#sidebar').length)
       {
+        console.log(12);
         // do nothing because clicked on sidebar
+        // try to scroll or something else
       }
       else if($(_e.target).is('#sidebar'))
       {
+        console.log(13);
         // do nothing because clicked on sidebar
         // sidebar is empty place of back
         pwaSidebarHide();
       }
       else if(!$('#sidebar').is(":visible"))
       {
+        pwaSidebarHide();
+        console.log(14);
         // do nothing because its hide!
       }
       else
       {
-        pwaSidebarShow();
+        console.log(15);
+        pwaSidebarHide();
       }
     }
+  });
+
+  $('#sidebar a[href]').on('click', function()
+  {
+      setTimeout(function()
+      {
+        console.log('hide after click');
+        pwaSidebarHide();
+      }, 200);
   });
 }
 
 
 function pwaSidebarShow()
 {
-  $('body').attr('data-aside', '');
+  $('body').attr('data-aside', 'open');
   $('#sidebar').show(100).attr('data-active', "");
   $('#pageHeader .pwa .hamburger').attr('data-active', "");
 }
