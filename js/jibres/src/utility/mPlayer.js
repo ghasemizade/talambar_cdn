022d9a6ae2039ec8819b142469548191253f9034
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-19 18:06:55 +0330

    show play pause

--- js/jibres/src/utility/mPlayer.js
+++ js/jibres/src/utility/mPlayer.js
@@ -1,15 +1,19 @@
 
 function mPlayer()
 {
-	$('video').off('click').on('click', function()
+	$('[data-magicbox] video').off('click.video').on('click.video', function()
 	{
+		var videoFrame = $(this).parents('[data-magicbox]');
+
 		if(this.paused)
 		{
 			this.play()
+			videoFrame.attr('data-playing', '');
 		}
 		else
 		{
 			this.pause()
+			videoFrame.attr('data-playing', null);
 		}
 	});
 }
