369917b209fc7d717519e018ba316950fe16694e
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-24 13:28:36 +0330

    remove console

--- js/jibres/src/utility/fitNumber.js
+++ js/jibres/src/utility/fitNumber.js
@@ -47,7 +47,7 @@ function humanMin(s)
     s = Math.floor( s % 60 );
     s = s >= 10 ? s : '0' + s;
     r = fitNumber(m) + ':' + fitNumber(s);
-    console.log(s);
+
     if(s === '00' && urlLangFa())
     {
       r += fitNumber(s);
