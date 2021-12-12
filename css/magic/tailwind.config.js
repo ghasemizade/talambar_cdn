1f2be623432780d8b8941005da13260cc22a06ad
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-12-12 16:53:29 +0330

    add font

--- css/magic/tailwind.config.js
+++ css/magic/tailwind.config.js
@@ -3,6 +3,28 @@ module.exports = {
     "./src/templates/**/*.{html,js}"
   ],
   theme: {
+    screens: {
+      'sm': {'min': '640px'},
+      'md': {'min': '768px'},
+      'lg': {'min': '1024px'},
+      // 'xl': {'min': '1280px'},
+      // '2xl': {'min': '1536px'},
+    },
+    fontFamily: {
+      'sans': '"LatoLatinWeb", IRANYekan, ui-sans-serif, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif',
+      'siftal': 'siftal',
+      'mono': ['ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'],
+      'IRANSansX': 'IRANSansX',
+      'IRANYekan': 'IRANYekan',
+      'LatoLatinWeb': 'LatoLatinWeb',
+      'Vazir': 'Vazir',
+      'tahoma': 'tahoma',
+      'GanjNamehSans': 'GanjNamehSans',
+      'NikaWeb': 'NikaWeb',
+      'BehdadWeb': 'BehdadWeb',
+      'WebNastaliq': 'WebNastaliq',
+      'Lalezar': 'Lalezar',
+    },
     extend: {},
   },
   plugins: [
