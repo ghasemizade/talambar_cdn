ae677d57fc929aa50d368111fa0e9b6eddda9cbd
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-12-14 18:41:54 +0330

    some fix

--- js/jibres/src/storePanel/storePanel.js
+++ js/jibres/src/storePanel/storePanel.js
@@ -499,12 +499,12 @@ function addNewRecord_ProductList(_table, _product, _append)
   var factorType = $('body').attr('data-page');
 
   var trEmpty   = '<tr>';
-  trEmpty       += '<td class="cellIndex text-xs"></td>';
-  trEmpty       += '<td class="cellTitle text-xs"></td>';
+  trEmpty       += '<td class="cellIndex text-sm"></td>';
+  trEmpty       += '<td class="cellTitle text-sm"></td>';
   trEmpty       += '<td class="cellCount"></td>';
   trEmpty       += '<td class="cellPrice"></td>';
   trEmpty       += '<td class="cellDiscount"></td>';
-  trEmpty       += '<td class="cellTotal text-sm"></td>';
+  trEmpty       += '<td class="cellTotal"></td>';
   trEmpty       += '</tr>';
   var newRecord = $(trEmpty);
   var cuRow     = _table.find('tr').length;
@@ -517,13 +517,13 @@ function addNewRecord_ProductList(_table, _product, _append)
     {
       myQuantity = 1;
     }
-    var htmlPName     = _product.title;
+    var htmlPName = _product.title;
     // if allow to edit, use iframe to edit link
     if(_product.editlink)
     {
-      htmlPName     = '<a data-fancybox data-type="iframe" target="_blank" href="' + _product.editlink + '">' + _product.title + '</a>';
+      htmlPName = '<a data-fancybox data-type="iframe" target="_blank" href="' + _product.editlink + '">' + htmlPName + '</a>';
     }
-    htmlPName         += '<input type="hidden" name="products[]" class="hidden" value="' + _product.id + '">';
+    htmlPName += '<input type="hidden" name="products[]" class="hidden" value="' + _product.id + '">';
 
     var htmlPCount    = '<input class="input count" type="number" name="count[]" autocomplete="off" min="0" max="1000000000" step="any" placeholder="-" value="'+ myQuantity +'">';
     var htmlPPrice    = '<input class="input price" type="number" name="price[]" autocomplete="off" min="0" max="1000000000" value="' + _product.price +'">';
