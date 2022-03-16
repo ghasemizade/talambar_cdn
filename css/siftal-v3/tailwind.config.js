3698e87dc7191b0216a94bb2f730bbdb8de02560
Javad Adib <Mr.Javad.Adib@gmail.com>
2022-03-16 14:42:11 +0330

    update font family

--- css/siftal-v3/tailwind.config.js
+++ css/siftal-v3/tailwind.config.js
@@ -1,6 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'sans': '"LatoLatinWeb", IRANYekan, ui-sans-serif, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif',
+      'siftal': 'siftal',
+      'mono': ['ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'],
+      'IRANSansX': 'IRANSansX',
+      'IRANYekan': 'IRANYekan',
+      'LatoLatinWeb': 'LatoLatinWeb',
+      'Vazir': 'Vazir',
+      'tahoma': 'tahoma',
+      'GanjNamehSans': 'GanjNamehSans',
+      'NikaWeb': 'NikaWeb',
+      'BehdadWeb': 'BehdadWeb',
+      'WebNastaliq': 'WebNastaliq',
+      'Lalezar': 'Lalezar',
+    },
     extend: {},
   },
   plugins: [],
