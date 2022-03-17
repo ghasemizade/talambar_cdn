071277dbea9235d3135291c63ec15300b71612d5
Javad Adib <Mr.Javad.Adib@gmail.com>
2022-03-17 17:04:27 +0330

    seperate tw and magic

--- css/siftal-v3/tailwind.config.js
+++ css/siftal-v3/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./style/**/*.{html,js}"],
+  content: ["./tw/**/*.{html,js}"],
   theme: {
     fontFamily: {
       'sans': '"LatoLatinWeb", IRANYekan, ui-sans-serif, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif',
